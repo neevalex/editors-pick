edf62babd46d9a92b461c04a4fc9060f3c54d639
neevalex <neevalex@gmail.com>
2019-03-28 13:13:20 +0200

    clearing up

--- public/class-editors-pick-public.php
+++ public/class-editors-pick-public.php
@@ -61,18 +61,6 @@ class Editors_Pick_Public {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Editors_Pick_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Editors_Pick_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/editors-pick-public.css', array(), $this->version, 'all' );
 
 	}
@@ -84,20 +72,8 @@ class Editors_Pick_Public {
 	 */
 	public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Editors_Pick_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Editors_Pick_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/editors-pick-public.js', array( 'jquery' ), $this->version, false );
-
+		// wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/editors-pick-public.js', array( 'jquery' ), $this->version, false ); // No need to use JS for NOW.
+        
 	}
 
 }
